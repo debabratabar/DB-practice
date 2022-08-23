918d3ca406d29e4bc0b5a6ffee902f29fba05604
debabrata.bar1999@gmail.com <debabrata.bar1999@gmail.com>
2022-08-23 06:16:16 +0000

    practiceNotebook

---
+++ Databricks-basics-learning (1).py
@@ -0,0 +1,21 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC ## this cell is not an executable cell , its a markdown cell
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC select 14+4 ;
+
+# COMMAND ----------
+
+print("My name is Debabrata Bar")
+
+# COMMAND ----------
+
+import math
+math.cos(1)
+
+# COMMAND ----------
+
+
